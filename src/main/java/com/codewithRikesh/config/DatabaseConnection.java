5839b36a4a1c7f028832ebe8ceb8ed21f2913fd5
Rikesh Neupane <neupane857@gmail.com>
2026-02-15 22:19:20 -0600

    Adding secrets to env variables

--- src/main/java/com/codewithRikesh/config/DatabaseConnection.java
+++ src/main/java/com/codewithRikesh/config/DatabaseConnection.java
@@ -6,12 +6,16 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/CarRentalDB";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "password";
+    //private static final String URL = "jdbc:mysql://localhost:3306/CarRentalDB";
+    //private static final String USERNAME = "root";
+    //private static final String PASSWORD = "password";
+
 
     public static Connection connect(){
         Connection connection = null;
+        String URL = requireEnv("DB_URL");
+        String USERNAME = requireEnv("DB_USERNAME");
+        String PASSWORD = requireEnv("DB_PASSWORD");
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("Database connected successfully!");
@@ -20,5 +24,15 @@ public class DatabaseConnection {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    private static String requireEnv(String key) {
+        String value = System.getenv(key);
+
+        if (value == null || value.isBlank()) {
+            System.out.println("Missing required environment variable" + key);
+            throw new IllegalStateException("Missing required environment variable: " + key);
+        }
+        return value;
     }
 }
